fe96051be3eb00d96f4312b784ee6426cef7be8e
RubenVG <rmvang01@gmail.com>
2022-12-03 22:51:25 +0100

    Added thank you dialog for landing page

--- ruben_nl/src/components/HelloWorld.vue
+++ ruben_nl/src/components/HelloWorld.vue
@@ -9,7 +9,8 @@
       v-model="drawer"
       id="nav_drawer"
       absolute
-      temporary>
+      temporary
+      elevation="24">
       <!--  -->
     </v-navigation-drawer>
 
@@ -19,32 +20,57 @@
       <v-app-bar-nav-icon id="header_element" @click="drawer = !drawer"></v-app-bar-nav-icon>
       
       <v-spacer></v-spacer>
-
       
       <a href="https://github.com/RubenvanGemeren" target="_blank"><v-icon id="header_element" size="large" class="mr-4">mdi-github</v-icon></a>
       <a href="https://www.linkedin.com/in/ruben-van-gemeren-1524a6151/?locale=en_US" target="_blank"><v-icon id="header_element" size="large" class="mx-4">mdi-linkedin</v-icon></a>
       <a href="https://steamcommunity.com/id/rubini9/" target="_blank"><v-icon id="header_element" size="large" class="mx-4">mdi-steam</v-icon></a>
+      <v-btn
+            rounded="lg"
+            variant="tonal"
+        >
+          Thank you
+          <v-dialog
+          v-model="dialog"
+          activator="parent"
+          id="thank_you_dialog"
+        >
+          <v-card
+            id="thank_you_card">
+            <v-card-text
+              >
+              <p id="thank_you_text">Before you begin I want to thank you for visiting this website, I worked hard on it and I think it's <strong id="cool_text">pretty cool</strong>. It will be a ongoing project so you might wanna check in once in a while to see new ideas being worked on!</p>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="success" block @click="dialog = false">Close</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-btn>
 
     </v-app-bar>
 
     <v-main id="main_block">
       <v-container
+        class="rounded-xl"
+        fluid
+        id="container_1"
+        >
+        <p id="intro_text">Hi, i'm</p>
+        <h1 id="name_text" class="display">Ruben</h1>
+        <h1 id="name_text_small" class="display">van</h1>
+        <h1 id="name_text" class="display">Gemeren</h1>
+        <h1 id="title_text">Dutch Software Developer // color green enthusiast</h1>
+    </v-container>
+      
+    <v-container
       class="rounded-xl"
       fluid
-      id="container_1"
-      >
-      <p id="hi_text">Hi, i'm</p>
-      <h1 id="name_text" class="display">Ruben</h1>
-      <h1 id="name_text_small" class="display">van</h1>
-      <h1 id="name_text" class="display">Gemeren</h1>
-      <h1 id="title_text">Dutch Software Developer // color green enthusiast</h1>
-    </v-container>
-      <v-container
-      fluid
-      class=""
       id="container_2"
       >
-      <h1 class="display">Second part of the page</h1>
+      <p id="intro_text" class="display">Who am I?</p>
+      <p>My name is <strong>Ruben</strong>, I'm <i>21 years</i> old and live in one of the biggest cities in the Netherlands; Rotterdam.</p>
+      <p></p>
+      <p></p>
     </v-container>
       <v-container
       fluid
@@ -61,6 +87,12 @@
   import '@/assets/myscss/myscss.scss';
 
   export default {
-    data: () => ({ drawer: null }),
+    data: () => ({ 
+      drawer: null ,
+      // The alert can be used for a later event.
+      alert: true,
+      dialog: false,
+    }),
+    
   }
 </script>
