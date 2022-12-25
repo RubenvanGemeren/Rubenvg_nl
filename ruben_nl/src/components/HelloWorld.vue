a99c26af7d6601fd6b8d7278fba1771a51d1989c
RubenVG <rmvang01@gmail.com>
2022-12-26 00:56:57 +0100

    experience cards design with logos

--- ruben_nl/src/components/HelloWorld.vue
+++ ruben_nl/src/components/HelloWorld.vue
@@ -1,5 +1,3 @@
-
-
 <template>
 
   <v-app 
@@ -58,10 +56,10 @@
         fluid
         id="container_1"
         >
-        <p id="intro_text">Hi, i'm</p>
-        <h1 id="name_text" class="display">Ruben</h1>
-        <h1 id="name_text_small" class="display">van</h1>
-        <h1 id="name_text" class="display">Gemeren</h1>
+        <p class="intro_text">Hi, i'm</p>
+        <h1 class="display name_text">Ruben</h1>
+        <h1 class="display name_text_small">van</h1>
+        <h1 class="display name_text">Gemeren</h1>
         <h1 id="title_text">Dutch Software Developer // color green enthusiast</h1>
     </v-container>
       
@@ -70,15 +68,15 @@
       fluid
       id="container_2"
       >
-      <p id="intro_text" class="display">Who am I?</p>
-      <div id="intro_container">
+      <p class="display intro_text">Who am I?</p>
+      <div class="intro_container">
 
         <div>
           <p id="body_text">My name is <strong>Ruben</strong>, i'm <i>21 years</i> old and i'm from the Netherlands. As of autumn of 2022 i'm offically a <strong>bachelor of science</strong> after completing my computer science study at the Hogeschool Rotterdam, which means i'm finally a free man (kinda).</p>
           
           <br>
           
-          <p id="body_text">Although I can say goodby to my old classmates, I hope this is just <strong>one</strong> of the many steps i'll take in my educational carreer. My plan is to follow a master and dive deeper in the world of software development and the architecture of systems and applications.</p>
+          <p id="body_text">Although I can say goodbye to my old classmates, I hope this is just <strong>one</strong> of the many steps i'll take in my educational carreer. My plan is to follow a master and dive deeper in the world of software development and the architecture of systems and applications.</p>
           
           <br>
           
@@ -112,12 +110,13 @@
       </div>
 
     </v-container>
-      <v-container
+
+    <v-container
       fluid
       class=""
       id="container_3"
       >
-      <p id="intro_text" class="display">My Experience</p>
+      <p class="display intro_text">My Experience</p>
       <div class="container_3_box">
         <v-card
           class="card_experience card_webdev rounded-lg"
@@ -131,7 +130,8 @@
             <p>Not just this website!</p>
           </v-card-subtitle>
           <v-card-text>
-            Working with languages like <strong>PHP</strong> using <strong>Laravel</strong>, <strong>Javascript</strong> transformed with <strong>Typescript</strong> and modern frontend frameworks like <strong>Vue</strong> enchanched with <strong>Vuetify</strong>. Throughout the years I've worked on and developed both educational and professional level applications. Projects include <strong>CRM</strong>, <strong>ERP</strong> and <strong>Integration</strong> applications, as well as a <strong>restaurant system </strong> and <strong>web scraper</strong> used for national new sites.
+            <img src="../assets/logos/webdev_icons.png" alt="webdev icons" class="experience_card_img">
+            <!-- Working with languages like <strong>PHP</strong> using <strong>Laravel</strong>, <strong>Javascript</strong> transformed with <strong>Typescript</strong> and modern frontend frameworks like <strong>Vue</strong> enchanched with <strong>Vuetify</strong>. Throughout the years I've worked on and developed both educational and professional level applications. Projects include <strong>CRM</strong>, <strong>ERP</strong> and <strong>Integration</strong> applications, as well as a <strong>restaurant system </strong> and <strong>web scraper</strong> used for national new sites. -->
           </v-card-text>
           <v-card-actions>
             <v-btn>Click me</v-btn>
@@ -150,7 +150,8 @@
             Changing the world!
           </v-card-subtitle>
           <v-card-text>
-            A big part of my personal development during my education was learning about and working with <strong>artificial intelligence</strong> AKA AI. Besides spending more than half a year learning about what AI is and how it works I have also created both a <strong>machine learning</strong> and <strong>deep learing</strong> algorithm to get used to some of the most popular frameworks. <strong>Python</strong>, <strong>Tensorflow</strong>, <strong>Pandas</strong> and more.
+            <img src="../assets/logos/AI_icons.png" alt="AI icons" class="experience_card_img">
+            <!-- A big part of my personal development during my education was learning about and working with <strong>artificial intelligence</strong> AKA AI. Besides spending more than half a year learning about what AI is and how it works I have also created both a <strong>machine learning</strong> and <strong>deep learing</strong> algorithm to get used to some of the most popular frameworks. <strong>Python</strong>, <strong>Tensorflow</strong>, <strong>Pandas</strong> and more. -->
               
           </v-card-text>
           <v-card-actions>
@@ -170,32 +171,14 @@
             Whatever you need boss!
           </v-card-subtitle>
           <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </v-card-text>
-          <v-card-actions>
-            <v-btn>Click me</v-btn>
-          </v-card-actions>
-        </v-card>
-        <v-card
-          class="card_experience card_research rounded-lg"
-          color="purple"
-          elevation="20"
-        >
-          <v-card-title>
-            Reseach
-          </v-card-title>
-          <v-card-subtitle>
-          And development of course!
-          </v-card-subtitle>
-          <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <img src="../assets/logos/programming_icons.png" alt="programming icons" class="experience_card_img">
+            <!-- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. -->
           </v-card-text>
           <v-card-actions>
             <v-btn>Click me</v-btn>
           </v-card-actions>
         </v-card>
       </div>
-
     </v-container>
     </v-main>
   </v-app>
